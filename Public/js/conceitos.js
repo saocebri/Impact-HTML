1d05a4eb3e9088e0533b869ee0ded48f861bbf56
saocebri <albacarlos2012@gmail.com>
2022-02-18 23:15:43 -0300

    Java

---
+++ Public/js/conceitos.js
@@ -0,0 +1,27 @@
+/*
+Tipos de Dados
+--------------------------------
+number
+string
+boolean
+null
+undefined
+object
+*/
+"use strict"
+
+let nome = "10" * 10; // O java consegue converter um string(numeros) em variavel tipo numero
+let numero = 10;
+let boolean = true;
+let divisao = 10 / 2;
+
+const COLOR_RED = "#ff0000"; // Declaração de variavel constante (nunca muda)
+
+
+
+console.log("tipo:", typeof nome, nome);
+console.log("tipo:", typeof numero, numero);
+console.log("tipo:", typeof boolean, boolean);
+console.log("tipo:", typeof decimal, divisao);
+
+
---
+++ Public/js/hello-word.js
@@ -0,0 +1,8 @@
+console.log("Meu Primeiro Script");
+
+var nome = "Paulo";
+if (nome === '') {
+    console.log('Nome Paulo não existe')
+} else {
+    console.log("Olá", nome);
+}
