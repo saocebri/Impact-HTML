ef62330a848e39cec6fad363fd370f36813dfe3a
saocebri <albacarlos2012@gmail.com>
2022-02-21 20:05:19 -0300

    1

--- Public/js/conceitos.js
+++ Public/js/conceitos.js
@@ -1,19 +1,21 @@
 /*
 Tipos de Dados
 --------------------------------
-number
-string
-boolean
-null
-undefined
-object
+number = Numeros inteiros e decimais
+string =  texto
+boolean = true e false
+null = nenhuma informação
+undefined = é uma variavel é declarada, mas não tem valor atribuido
+object = 
 */
 "use strict"
 
 let nome = "10" * 10; // O java consegue converter um string(numeros) em variavel tipo numero
 let numero = 10;
 let boolean = true;
-let divisao = (10 / 2);
+let encontrandoTipo = typeof nome;
+let idade = Number ("25") // forma de converter uma string para number
+let idade2 = String(25) //Forma de converter um number em string
 
 const COLOR_RED = "#ff0000"; // Declaração de variavel constante (nunca muda)
 
@@ -22,6 +24,9 @@ const COLOR_RED = "#ff0000"; // Declaração de variavel constante (nunca muda)
 console.log("tipo:", typeof nome, nome);
 console.log("tipo:", typeof numero, numero);
 console.log("tipo:", typeof boolean, boolean);
-console.log("tipo:", typeof decimal, divisao);
+console.log("tipo:", typeof idade, idade);
+console.log("tipo:", typeof idade2, idade2);
+console.log("tipo:", encontrandoTipo);
+
 
 
