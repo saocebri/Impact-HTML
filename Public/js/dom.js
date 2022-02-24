de6f0853f66ee6c94f576a95e0def527da035773
saocebri <albacarlos2012@gmail.com>
2022-02-23 22:57:02 -0300

    Buscado o objeto do html(caixa de texto) com evento click do botão

--- Public/js/dom.js
+++ Public/js/dom.js
@@ -1,1 +1,32 @@
-alert("Olá Mundo");
+"use strict"
+// alert("Olá Mundo");
+
+
+//Buscado o objeto do html(caixa de texto) com evento click do botão
+
+let botao = document.querySelector('.btn');
+botao.addEventListener('click', (event) => {
+    let objNome = document.querySelector('.input-name');
+    let nome = objNome.value;
+    let divError = document.querySelector('.error');
+    let buscaCarateres = RegExp('@', 'i')
+    let testReg = buscaCarateres.test(nome);
+
+    if (nome === '') {
+        divError.innerHTML = 'Erro: Campo Vazio';
+        // objNome.classList('m-error');
+    }
+    else if (nome.length < 6) {
+        divError.innerHTML = 'Nome não pode ser menor que 6 caracteres';
+    }
+    else if (testReg) {
+        divError.innerHTML = 'Não pode usar o @'
+    }
+    else {
+        divError.innerHTML = 'Tudo certo';
+    }
+    event.preventDefault();
+});
+
+
+
